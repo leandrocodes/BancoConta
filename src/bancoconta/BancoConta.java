0da42392fa051dc03ee29d04efd7a5a016e9df23
Leandro Viana <leaxviana140@gmail.com>
2018-10-16 21:17:39 -0400

    case 2 e case 3 do menu

--- src/bancoconta/BancoConta.java
+++ src/bancoconta/BancoConta.java
@@ -23,37 +23,63 @@ public class BancoConta {
         ContaDAO contaDAO = new ContaDAO();
         FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
         Scanner aux = new Scanner(System.in);
-        
-        
-        while(true){
+
+        while (true) {
             System.out.println("Inserir uma Conta - Digite 1");
             System.out.println("Buscar uma Conta - Digite 2");
             System.out.println("Sair - Digite 3");
-        
-        int op = aux.nextInt();
-        boolean exit = false;
-        
-        switch(op){
-            case 1:
-                   System.out.println("Digite o número da Agência: ");
-                   int numero = aux.nextInt();
-                   Agencia agencia = agenciaDAO.getAgencia(numero);
-                   
-                   System.out.println("Digite o CPF do cliente: ");
-                   String cpf = aux.nextLine();
-                   Cliente cliente = clienteDAO.getCliente(cpf);
-                   
-                   if(agencia == null || cliente == null){
-                       System.out.println("Não foi possível criar, cliente ou agência inexistentes");
-                   }else{
-                       System.out.println("Digite o número da conta: ");
-                       int nc = aux.nextInt();
-                       
-                       Conta ct = contaDAO.getConta(nc);
-                   }
+
+            int op = aux.nextInt();
+            boolean exit = false;
+
+            switch (op) {
+                case 1:
+                    System.out.println("Digite o número da Agência: ");
+                    int numero = aux.nextInt();
+                    Agencia agencia = agenciaDAO.getAgencia(numero);
+
+                    System.out.println("Digite o CPF do cliente: ");
+                    String cpf = aux.nextLine();
+                    Cliente cliente = clienteDAO.getCliente(cpf);
+
+                    if (agencia == null || cliente == null) {
+                        System.out.println("Não foi possível criar, cliente ou agência inexistentes");
+                    } else {
+                        System.out.println("Digite o número da conta: ");
+                        int nc = aux.nextInt();
+
+                        Conta ct = contaDAO.getConta(nc);
+
+                        if (ct == null) {
+                            Conta c = new Conta(nc, 0, agencia, cliente);
+                            contaDAO.addConta(c);
+                        } else {
+                            System.out.println("Conta bancária já existente");
+                        }
+
+                    }
+                    break;
+                case 2:
+                    System.out.println("Digite o número da conta: ");
+                    int contaBusca = aux.nextInt();
+                    Conta c = contaDAO.getConta(contaBusca);
+
+                    if (c != null) {
+                        System.out.println("Número: " + c.getNumero());
+                        System.out.println("Saldo: " + c.getSaldo());
+                        System.out.println("Nome do cliente: " + c.getCliente().getNome());
+                    }
+
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+            }
+            if (exit) {
+                break;
+            }
         }
-        }
-        
+
     }
-    
+
 }
