4b0b7cfad1b04afdfaa4ea847763147874c5e6fc
Leandro Viana <leaxviana140@gmail.com>
2018-10-16 21:00:07 -0400

    inicializa objetos dao na main

--- src/bancoconta/BancoConta.java
+++ src/bancoconta/BancoConta.java
@@ -5,6 +5,12 @@
  */
 package bancoconta;
 
+import bancoconta.DAO.AgenciaDAO;
+import bancoconta.DAO.ClienteDAO;
+import bancoconta.DAO.ContaDAO;
+import bancoconta.DAO.FuncionarioDAO;
+import java.util.Scanner;
+
 /**
  *
  * @author leand
@@ -12,6 +18,30 @@ package bancoconta;
 public class BancoConta {
 
     public static void main(String[] args) {
+        ClienteDAO clienteDAO = new ClienteDAO();
+        AgenciaDAO agenciaDAO = new AgenciaDAO();
+        ContaDAO contaDAO = new ContaDAO();
+        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+        Scanner aux = new Scanner(System.in);
+        
+        
+        while(true){
+            System.out.println("Inserir uma Conta - Digite 1");
+            System.out.println("Buscar uma Conta - Digite 2");
+            System.out.println("Sair - Digite 3");
+        
+        int op = aux.nextInt();
+        boolean exit = false;
+        
+        switch(op){
+            case 1:
+                   System.out.println("Digite o número da Agência: ");
+                   int numero = aux.nextInt();
+                   Agencia agencia = agenciaDAO.getAgencia(numero);
+                   
+                   System.out.println("");
+        }
+        }
         
     }
     
